18215622262dcc64d538bfad572016b51c4e25e1
pedro.goncalves <pedro.goncalves@engineeringhub.man>
2025-07-08 22:49:18 +0100

    qmk setup working v1

---
+++ keyboards/crkbd/rules.mk
@@ -0,0 +1,13 @@
+# Build Options
+#   change yes to no to disable
+#
+
+DEFAULT_FOLDER = crkbd/rev1
+
+RGBLIGHT_ENABLE = yes
+
+TAP_DANCE_ENABLE = yes
+
+LAYER_LOCK_ENABLE = yes
+
+KEY_OVERRIDE_ENABLE = yes
